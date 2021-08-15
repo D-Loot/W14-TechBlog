2955c494b71901868fe432d672dfbdbc03a56988
Dan Looten <danwlooten@gmail.com>
2021-08-15 01:35:03 -0500

    Added delete button functionality and added in username/date info to comments/posts

--- public/js/comment.js
+++ public/js/comment.js
@@ -1,12 +1,8 @@
 // the purpose of this js is to receive comment posts and comment deletions.
 const newFormHandler = async (event) => {
-  const post_id = document.querySelector('#data-id');
+  const post_id = document.querySelector('#post-id').value;
   const title = document.querySelector('#comment-title').value.trim();
   const description = document.querySelector('#comment-desc').value.trim();
-
-  console.log("Title: ",title);
-  console.log("Description: ",description);
-  console.log("Post_id: ",post_id);
 
   if (title && description&& post_id) {
     const response = await fetch(`/api/comments`, {
@@ -16,9 +12,6 @@ const newFormHandler = async (event) => {
         'Content-Type': 'application/json',
       },
     });
-    console.log("--------------------------")
-    console.log(response);
-    console.log("--------------------------")
     if (response.ok) {
       document.location.replace(`/post/${post_id}`);
     } else {
